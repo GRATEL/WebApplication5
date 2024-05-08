78ef9719d0e9f70fd8e86191c6ece35d2bc9196d
Home <Home@DESKTOP-O06C846>
2024-05-08 06:25:17 -0700

    Add project files.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication5.NewFolder;
+
+namespace WebApplication5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "API is working";
+        }
+        [HttpPost]
+        public ActionResult Post(Person person) {
+            return Ok($"Received:{person.Name}");
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication5.NewFolder
+{
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+}
